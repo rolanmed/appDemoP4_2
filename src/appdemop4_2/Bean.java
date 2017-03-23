d5b6dda5170a28c7fb857d9e272f91d089b94fe0
rolanmed <rolanmed@gmail.com>
2017-03-23 07:48:59 -0600

    Se agrego clase Empleado

--- src/appdemop4_2/Bean.java
+++ src/appdemop4_2/Bean.java
@@ -10,5 +10,45 @@ package appdemop4_2;
  * @author Rolando
  */
 public class Bean {
+    private int a;
+    private double b;
+    private String c;
+
+     public Bean() {
+        this.a = 0;
+        this.b = 0.0;
+        this.c = "";
+    }
+     
+    public Bean(int a, double b, String c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public String getC() {
+        return c;
+    }
+
+    public void setC(String c) {
+        this.c = c;
+    }
+    
     
 }
